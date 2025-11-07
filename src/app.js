f5fea20ee0b3f2f075e618fca9e1903a2bb45399
carlos <apolayamendozacarlos@gmail.com>
2025-11-07 16:53:07 -0500

    First Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../")));
+
+// Levantar el index.html desde el puerto 3000
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server corriendo en: http://localhost:${PORT}`);
+});
